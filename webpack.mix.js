0d732bcd36f651fea448bc888ea28570fdb8deb4
Thiago Braga <thibraga06@gmail.com>
2019-10-08 01:00:30 -0300

    Arquivos JS de cada página agora na pasta pages
    - Movidos arquivos JS de cada página para `pages`
    - Criado arquivo `Usuarios.js` em `pages`
    - Versionando alterações no `mix-manifest.json`
    
    Ref #168229016

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,8 +2,9 @@ const mix = require('laravel-mix')
 const sassOptions = { outputStyle: 'compressed' }
 
 mix.js('resources/js/App.js', 'public/js')
-  .js('resources/js/pages/Home.js', 'public/js')
-  .js('resources/js/pages/Cidades.js', 'public/js')
+  .js('resources/js/pages/Home.js', 'public/js/pages')
+  .js('resources/js/pages/Cidades.js', 'public/js/pages')
+  .js('resources/js/pages/Usuarios.js', 'public/js/pages')
 
   .sass('resources/sass/app.scss', 'public/css', { sassOptions })
   .sass('resources/sass/pages/login.scss', 'public/css', { sassOptions })
