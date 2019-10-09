ddc47f03aebf6799145876cc679de2053ab280e4
Thiago Braga <thibraga06@gmail.com>
2019-10-09 13:48:50 -0300

    Blades do Datatables renomeadas e movidas
    ...para resources/views/vendor/datatables

---
+++ resources/views/vendor/datatables/js.blade.php
@@ -0,0 +1,7 @@
+<!-- Datatables -->
+<script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.0/js/dataTables.buttons.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.0/js/buttons.bootstrap4.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.0/js/buttons.colVis.min.js"></script>
+<script src="{{ asset('vendor/datatables/buttons.server-side.js') }}"></script>
