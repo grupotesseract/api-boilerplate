40a6922bfe4fd46c05b415115f5791f45e0d579b
Thiago Braga <thibraga06@gmail.com>
2019-10-14 06:25:19 -0300

    Melhorias nos botões de ações do Datatables

--- resources/views/usuarios/datatables_actions.blade.php
+++ resources/views/usuarios/datatables_actions.blade.php
@@ -1,13 +1,15 @@
 {!! Form::open(['route' => ['usuarios.destroy', $id], 'method' => 'delete']) !!}
-    <a href="{{ route('usuarios.show', $id) }}" class='btn btn-sm btn-ghost-primary'>
+    <a href="{{ route('usuarios.show', $id) }}"
+        class='btn btn-sm btn-success'>
         <i class="fa fa-eye"></i>
     </a>
-    <a href="{{ route('usuarios.edit', $id) }}" class='btn btn-sm btn-ghost-primary'>
+    <a href="{{ route('usuarios.edit', $id) }}"
+        class='btn btn-sm btn-warning'>
         <i class="fa fa-edit"></i>
     </a>
     {!! Form::button('<i class="fa fa-trash"></i>', [
         'type' => 'submit',
-        'class' => 'btn btn-sm btn-ghost-danger',
+        'class' => 'btn btn-sm btn-danger',
         'onclick' => "return confirm('Você tem certeza que deseja excluir esse registro?')"
     ]) !!}
 {!! Form::close() !!}
