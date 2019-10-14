5538c60f80f961411d9668c033ec2e124b420be7
Thiago Braga <thibraga06@gmail.com>
2019-10-14 06:20:01 -0300

    Habilitando Livereload para PHP no env
    Agora é possível habilitar ou desabilitar o Livereload do BrowserSync
    para arquivos PHP, bastando alterar a variável de false para true e
    executar novamente o build.

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,20 +1,21 @@
 const mix = require('laravel-mix')
-const sassOptions = { outputStyle: 'compressed' }
+const browserSyncFiles = ['public/css/*.{css,js}']
 
 mix.js('resources/js/App.js', 'public/js')
   .js('resources/js/pages/Home.js', 'public/js/pages')
   .js('resources/js/pages/Cidades.js', 'public/js/pages')
   .js('resources/js/pages/Usuarios.js', 'public/js/pages')
 
-  .sass('resources/sass/app.scss', 'public/css', { sassOptions })
-  .sass('resources/sass/pages/login.scss', 'public/css', { sassOptions })
-  .sass('resources/sass/pages/welcome.scss', 'public/css', { sassOptions })
+  .sass('resources/sass/app.scss', 'public/css')
+  .sass('resources/sass/pages/login.scss', 'public/css/pages')
+  .sass('resources/sass/pages/welcome.scss', 'public/css/pages')
 
   .browserSync({
-    files: ['public/css/*.{css,js}'],
+    files: !process.env.LIVERELOAD_PHP
+      ? browserSyncFiles
+      : ['{app,config,public,resources/views}/**/*.php', ...browserSyncFiles],
     proxy: 'localhost',
     logSnippet: false,
-    ghostMode: false,
     notify: false,
     open: false,
     port: 3000,
