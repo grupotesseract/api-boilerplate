2b4f9568847c38284f63314be41b9f2778009aa2
Thiago Braga <thibraga06@gmail.com>
2020-07-31 15:55:55 -0300

    feat: Desabilita a criação de mix-manifest.json

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,25 +1,27 @@
 const mix = require('laravel-mix')
-const browserSyncFiles = ['public/css/*.{css,js}']
+const bsFiles = ['public/css/**/*.css', 'public/js/**/*.js}']
+
+// Disable mix-manifest.json
+Mix.manifest.refresh = _ => void 0
 
 mix.js('resources/js/App.js', 'public/js')
   .js('resources/js/pages/Cidades.js', 'public/js/pages')
-
   .sass('resources/sass/app.scss', 'public/css')
   .sass('resources/sass/pages/login.scss', 'public/css/pages')
-  .sass('resources/sass/pages/welcome.scss', 'public/css/pages')
 
-  .browserSync({
-    files: !process.env.LIVERELOAD_PHP
-      ? browserSyncFiles
-      : ['{app,config,public,resources/views}/**/*.php', ...browserSyncFiles],
-    proxy: 'localhost',
-    logSnippet: false,
-    notify: false,
-    open: false,
-    port: 3000,
-    ui: false
-  })
-
-  .webpackConfig({
-    devtool: !mix.inProduction() && 'inline-source-map'
-  })
+if (!mix.inProduction()) {
+  mix.webpackConfig({ devtool: 'source-map' })
+    .sourceMaps()
+    .disableNotifications()
+    .browserSync({
+      files: process.env.LIVERELOAD_PHP
+        ? ['{app,config,public,resources/{lang,views}}/**/*.php', ...bsFiles]
+        : bsFiles,
+      proxy: 'localhost',
+      logSnippet: false,
+      notify: false,
+      open: false,
+      port: 3000,
+      ui: false
+    })
+}
